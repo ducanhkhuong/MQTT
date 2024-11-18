43f48007704003ecbcd206cfa93f4671e73caefc
ducanhkhuong <duca3774@gmail.com>
2024-11-18 15:51:40 +0700

    update attribute QoS

--- lib/src/mqtt/broker.rs
+++ lib/src/mqtt/broker.rs
@@ -5,9 +5,16 @@ pub mod broker {
     use tokio::sync::mpsc;
     use tokio::sync::mpsc::UnboundedSender;
 
+    #[derive(Debug, PartialEq, Copy, Clone)]
+    pub enum Qos {
+        Low,   
+        Medium, 
+        High, 
+    }
+
     pub enum Command {
-        Publish { topic: String, message: String },
-        Subscribe { topic: String },
+        Publish { topic: String, message: String, qos: Qos },
+        Subscribe { topic: String, qos: Qos },
     }
 
     #[derive(Clone)]
@@ -17,31 +24,34 @@ pub mod broker {
 
     impl Broker {
         pub fn new() -> Self {
-            let (tx, mut rx) =
-                mpsc::unbounded_channel::<(Command, Option<UnboundedSender<(String, String)>>)>();
+            let (tx, mut rx) = mpsc::unbounded_channel::<(Command, Option<UnboundedSender<(String, String)>>)>();
 
             tokio::spawn({
                 async move {
-                    let mut subscribers: HashMap<String, Vec<UnboundedSender<(String, String)>>> =
-                        HashMap::new();
+                    let mut subscribers: HashMap<String, Vec<(UnboundedSender<(String, String)>, Qos)>> = HashMap::new();
 
                     while let Some((command, client_tx)) = rx.recv().await {
                         match command {
-                            Command::Subscribe { topic } => {
+
+                            Command::Subscribe { topic, qos } => {
                                 if let Some(subs) = subscribers.get_mut(&topic) {
                                     if let Some(tx) = client_tx {
-                                        subs.push(tx);
+                                        subs.push((tx, qos));
                                     }
-                                } else {
-                                    if let Some(tx) = client_tx {
-                                        subscribers.insert(topic, vec![tx]);
-                                    }
+                                } else if let Some(tx) = client_tx {
+                                    subscribers.insert(topic, vec![(tx, qos)]);
                                 }
                             }
-                            Command::Publish { topic, message } => {
+
+                            Command::Publish { topic, message, qos } => {
                                 if let Some(subs) = subscribers.get_mut(&topic) {
-                                    for sub in subs.iter_mut() {
-                                        let _ = sub.send((topic.clone(), message.clone()));
+                                    for (sub, sub_qos) in subs.iter_mut() {
+                                        if qos == Qos::Low || *sub_qos == Qos::Low {
+                                            let _ = sub.send((topic.clone(), message.clone()));
+                                            continue;
+                                        } else if qos == Qos::High || *sub_qos == Qos::High {
+                                            continue;
+                                        }
                                     }
                                 }
                             }
@@ -53,21 +63,20 @@ pub mod broker {
         }
 
         pub async fn start(&self, addr: &str) {
-            let listener = TcpListener::bind(addr).await.expect("False");
+            let listener = TcpListener::bind(addr).await.expect("Failed to bind");
             let mut counter = 0;
+            println!("[Broker]: waiting for connection");
             loop {
                 match listener.accept().await {
                     Ok((stream, _)) => {
                         counter += 1;
-                        println!("[Broker]: OK --- Client : {:?}", counter);
+                        println!("[Broker]: OK ---> Client {}", counter);
                         let tx = self.tx.clone();
                         tokio::spawn(async move {
                             handle_client(stream, tx).await;
                         });
                     }
-                    Err(e) => {
-                        eprintln!("{:?}", e);
-                    }
+                    Err(e) => eprintln!("Accept error: {}", e),
                 }
             }
         }
@@ -80,21 +89,19 @@ pub mod broker {
         let (mut reader, mut writer) = stream.into_split();
         let (client_tx, mut client_rx) = mpsc::unbounded_channel::<(String, String)>();
 
+        //gửi dữ liệu đến các client subscribe
         tokio::spawn({
             async move {
                 while let Some((topic, msg)) = client_rx.recv().await {
                     let formatted_message = format!("{} {}", topic, msg);
-                    if writer
-                        .write_all(formatted_message.as_bytes())
-                        .await
-                        .is_err()
-                    {
+                    if writer.write_all(formatted_message.as_bytes()).await.is_err() {
                         eprintln!("Error sending message to client");
                         break;
                     }
                 }
             }
         });
+
 
         let mut buffer = [0; 1024];
         loop {
@@ -103,9 +110,9 @@ pub mod broker {
                 Ok(n) => {
                     let request = String::from_utf8_lossy(&buffer[..n]);
 
-                    let parts: Vec<&str> = request.splitn(3, ' ').collect();
+                    let parts: Vec<&str> = request.split_whitespace().collect();
 
-                    if parts.len() < 2 {
+                    if parts.len() < 3 {
                         eprintln!("Invalid request: {}", request);
                         continue;
                     }
@@ -113,36 +120,22 @@ pub mod broker {
                     match parts[0] {
                         "SUBSCRIBE" => {
                             let topic = parts[1].to_string();
-                            let _ = tx.send((
-                                Command::Subscribe {
-                                    topic: topic.clone(),
-                                },
-                                Some(client_tx.clone()),
-                            ));
-                            println!("SUBSCRIBE: [Client] ---> [Broker] - Topic: {}", topic);
+                            let qos = parse_qos(parts[2]);
+                            let _ = tx.send((Command::Subscribe { topic: topic.clone(), qos },Some(client_tx.clone())));
+                            println!("SUBSCRIBE: [Client] ---> [Broker] - Topic: {}, QoS: {:?}", topic, qos);
                         }
                         "PUBLISH" => {
-                            if parts.len() < 3 {
+                            if parts.len() < 4 {
                                 eprintln!("Invalid publish request: {}", request);
                                 continue;
                             }
                             let topic = parts[1].to_string();
                             let message = parts[2].to_string();
-                            let _ = tx.send((
-                                Command::Publish {
-                                    topic: topic.clone(),
-                                    message: message.clone(),
-                                },
-                                None,
-                            ));
-                            println!(
-                                "PUBLISH  : [Client] ---> [Broker] - Topic: {}, Message: {}",
-                                topic, message
-                            );
+                            let qos = parse_qos(parts[3]);
+                            let _ = tx.send((Command::Publish { topic: topic.clone(), message: message.clone(), qos },None));
+                            println!("PUBLISH  : [Client] ---> [Broker] - Topic: {}, Message: {}, QoS: {:?}",topic, message, qos);
                         }
-                        _ => {
-                            eprintln!("Unknown command: {}", parts[0]);
-                        }
+                        _ => eprintln!("Unknown command: {}", parts[0]),
                     }
                 }
                 Err(e) => {
@@ -150,6 +143,15 @@ pub mod broker {
                     return;
                 }
             }
+        }
+    }
+
+    fn parse_qos(qos: &str) -> Qos {
+        match qos {
+            "0" => Qos::Low,
+            "1" => Qos::Medium,
+            "2" => Qos::High,
+            _   => Qos::Low,
         }
     }
 }
--- lib/src/mqtt/client.rs
+++ lib/src/mqtt/client.rs
@@ -1,7 +1,14 @@
-pub mod client{
+pub mod client {
     use std::io::{self, BufRead, Read, Write};
     use std::net::TcpStream;
     use std::thread;
+
+    #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+    pub enum Qos {
+        Low,
+        Medium,
+        High,
+    }
 
     #[derive(Debug, PartialEq, Eq)]
     pub enum Ex {
@@ -11,8 +18,8 @@ pub mod client{
     }
 
     pub enum Command {
-        Subscribe { topic: String },
-        Publish { topic: String, message: String },
+        Subscribe { topic: String, qos: Qos },
+        Publish   { topic: String, message: String, qos: Qos },
         Help,
     }
 
@@ -37,7 +44,7 @@ pub mod client{
 
             let mut stream_clone = self.stream.try_clone().expect("Failed to clone stream");
 
-            //luồng đọc dữ liệu từ broker nếu là client sub
+            //nhận dữ liệu tử broker nếu là client subscribe
             thread::spawn(move || loop {
                 let mut buffer = [0; 1024];
                 match stream_clone.read(&mut buffer) {
@@ -49,7 +56,7 @@ pub mod client{
                         let topic = parts.next().unwrap_or("Unknown Topic");
                         let message = parts.next().unwrap_or("Empty Message");
 
-                        println!("[Broker] ---> [Client] with topic '{}' : {}", topic, message);
+                        println!("[Broker] ---> [Client] : topic '{}' : {}", topic, message);
                     }
                     Err(e) => {
                         eprintln!("Error reading from stream: {}", e);
@@ -58,7 +65,8 @@ pub mod client{
                 }
             });
 
-            //luồng chính
+            // Main loop            
+            println!("Welcome mqtt:");
             Self::help();
 
             loop {
@@ -67,13 +75,11 @@ pub mod client{
                 let command = input.trim();
 
                 if command.is_empty() {
-                    println!("warning: empty ---> run : mqtt HELP");
                     continue;
                 }
 
                 match Self::parse_command(command) {
                     Ok(Ex::Empty) => {
-                        println!("warning: Invalid command ---> run : mqtt HELP");
                         continue;
                     }
                     Ok(Ex::Exit) => {
@@ -87,59 +93,106 @@ pub mod client{
             }
         }
 
-
-
         fn parse_command(command: &str) -> Result<Ex, String> {
             let parts: Vec<&str> = command.split_whitespace().collect();
 
             if parts.len() == 1 {
+                println!("warning: Invalid command ---> run : mqtt -H");
                 return Ok(Ex::Empty);
             }
             if parts.len() >= 2 && parts[0].to_lowercase() == "mqtt" {
                 return Ok(Ex::Mqtt);
-            } 
-            else if parts.len() == 1 && (parts[0].eq_ignore_ascii_case("exit")) {
+            } else if parts.len() == 1 && (parts[0].eq_ignore_ascii_case("exit")) {
                 Ok(Ex::Exit)
             } else {
-                Err("warning: Invalid command ---> run : mqtt HELP".to_string())
+                Err("warning: Invalid command ---> run : mqtt -H".to_string())
             }
         }
 
-
-
         fn handle_command(&mut self, command: &str) {
             let parts: Vec<&str> = command.split_whitespace().collect();
-            match parts[1].trim(){
-                "SUBSCRIBE" => {
+
+            let cmd = match parts[1]{
+                "SUBSCRIBE"|"--S" => {
+                    if parts.len() < 4 {
+                        println!("warning: Invalid command ---> run : mqtt -H");
+                        return;
+                    }
                     let topic = parts[2].to_string();
-                    let subscribe_request = format!("SUBSCRIBE {}", topic);
+                    let qos = self.parse_qos(parts[3]);
+                    Command::Subscribe { topic, qos }
+                }
+                "PUBLISH"|"--P" => {
+                    if parts.len() < 5 {
+                        println!("warning: Invalid command ---> run : mqtt -H");
+                        return;
+                    }
+                    let topic = parts[2].to_string();
+                    let message = parts[3].to_string();
+                    let qos = self.parse_qos(parts[4]);
+                    Command::Publish { topic, message, qos }
+                }
+                "HELP"|"--H" => {
+                    Command::Help
+                },
+                _ => {
+                    println!("warning: Invalid command ---> run : mqtt -H");
+                    return;
+                }
+            };
+
+            match cmd {
+                Command::Subscribe { topic, qos } => {
+
+                    let subscribe_request = format!("SUBSCRIBE {} {}", topic, qos as u8);
                     self.stream
                         .write_all(subscribe_request.as_bytes())
                         .expect("Failed to send subscribe request");
+                    println!("[Client] ---> [Broker] : SUBSCRIBE to '{}' with QoS '{:?}'", topic, qos);
+
                 }
-                "PUBLISH" => {
-                    let topic = parts[2].to_string();
-                    let message = parts[3].to_string();
-                    let publish_request = format!("PUBLISH {} {}", topic, message);
+                Command::Publish { topic, message, qos } => {
+
+                    let publish_request = format!("PUBLISH {} {} {}", topic, message, qos as u8);
                     self.stream
                         .write_all(publish_request.as_bytes())
                         .expect("Failed to send publish request");
-                    println!("[Client] ---> [Broker] with topic '{}' - message '{}' : send OK ", topic, message);
+                    println!("[Client] ---> [Broker] : PUBLISH to '{}' with message '{}' and QoS '{:?}'", topic, message, qos);
+
                 }
-                "HELP" => {
-                    Self::help();
-                }
-                _ => {
-                    println!("warning: Invalid command ---> run : mqtt HELP");
-                }
+
+                Command::Help => Self::help(),
             }
         }
 
         fn help() {
-            println!("Enter command : ");
-            println!("                mqtt SUBSCRIBE topic        ");
-            println!("                mqtt PUBLISH   topic message");
-            println!("                mqtt HELP                 ");
+            println!("                                                 ");
+            println!("[Usage]:        mqtt [OPTIONS] [COMMAND] [QOS]   ");
+            println!("                                                 ");    
+            println!("[OPTION] :                                       ");
+            println!("                  --S : SUBSCRIBE                ");
+            println!("                  --P : PUBLISH                  ");
+            println!("                  --H : HELP                     ");
+            println!("[COMMAND]:");
+            println!("             topic   : Specify topic             ");
+            println!("             message : Specify message           ");
+            println!("[QOS]    :");
+            println!("                   0 : QoS 0 - At most once      ");
+            println!("                   1 : QoS 1 - At least once     ");
+            println!("                   2 : QoS 2 - Exactly once      ");
+            println!("[EXAMPLE]:");
+            println!("                   mqtt --S topic qos            ");
+            println!("                   mqtt --P topic message qos    ");
+            println!("                   mqtt --H                      ");
+        }
+
+        fn parse_qos(&self, qos_str: &str) -> Qos {
+            match qos_str.to_lowercase().as_str() {
+                "0" => Qos::Low,
+                "1" => Qos::Medium,
+                "2" => Qos::High,
+                _ =>   Qos::Low,
+            }
         }
     }
 }
